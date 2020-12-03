17f201a454a23c0cd79517dc455c6b4cb6f5f47f
Cynthia-me <ayecythia@gmail.com>
2020-12-03 07:01:31 +0300

    initial commit

---
+++ madlibs.js
@@ -0,0 +1,8 @@
+let questions = 2;
+alert('There are only' + questions + 'questions');
+var noun = prompt('Please type in a noun');
+var verb = prompt('Please type in a verb');
+alert('Get ready');
+var sentence = 'The' + ' ' + noun + ' ' + verb + '.';
+document.write(sentence)
+
